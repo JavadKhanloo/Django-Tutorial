e9cd9ead4fbe35d1685ae86a553707d47b94410a
JavadKhanloo <khanloo.1414@gmail.com>
2024-01-25 17:50:58 +0330

    add user login and logout

--- 01-Online-Shop/accounts/forms.py
+++ 01-Online-Shop/accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class UserRegisterForm(forms.Form):
@@ -8,3 +9,31 @@ class UserRegisterForm(forms.Form):
     last_name = forms.CharField(max_length=200)
     password = forms.CharField(max_length=200)
     confirm_password = forms.CharField(max_length=200)
+
+    def clean_user_name(self):
+        user = self.cleaned_data['user_name']
+        if User.objects.filter(username=user).exists():
+            raise forms.ValidationError('این کاربر از قبل وجود دارد')
+        return user
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('این ایمیل از قبل وجود دارد')
+        return email
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('رمز عبورها یکسان نیستند')
+        elif len(confirm_password) < 8:
+            raise forms.ValidationError('رمز عبور حداقل باید 8 کاراکتر باشد')
+        elif not any(x.isupper() for x in confirm_password):
+            raise forms.ValidationError('رمز عبور باید حداقل شامل یک حرف بزرگ باشد')
+        return confirm_password
+
+
+class UserLoginForm(forms.Form):
+    user = forms.CharField(max_length=200)
+    password = forms.CharField(max_length=200)
--- 01-Online-Shop/accounts/views.py
+++ 01-Online-Shop/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegisterForm
+from .forms import *
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 def user_register(request):
@@ -9,10 +10,32 @@ def user_register(request):
         if form.is_valid():
             data = form.cleaned_data
             User.objects.create_user(username=data['user_name'], email=data['email'],
-                                     first_name=data['first_name'], last_name=data['last_name'], password=data['password'])
-        return redirect('home:home')
+                                     first_name=data['first_name'], last_name=data['last_name'],
+                                     password=data['confirm_password'])
+            return redirect('home:home')
     else:
         form = UserRegisterForm()
-
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            try:
+                user = authenticate(request, username=User.objects.get(email=data['user']), password=data['password'])
+            except:
+                user = authenticate(request, username=data['user'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home:home')
+    else:
+        form = UserLoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home:home')
